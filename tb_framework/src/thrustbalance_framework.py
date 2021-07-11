8ae9d68df162355c384048483b5ddb3059c252b6
kasraazizi <kasraazizi1375@gmail.com>
2021-07-11 11:34:10 +0430

    framework digit split modified

--- tb_framework/src/thrustbalance_framework.py
+++ tb_framework/src/thrustbalance_framework.py
@@ -11,7 +11,8 @@ import tbpwm
 
 #%%  
 ser = serial.Serial('/dev/ttyUSB0', 9600)
-
+ser.flushInput()
+ser.flushOutput()
 print("wait")
 while True :
     time.sleep(1)
@@ -24,45 +25,46 @@ while True :
 print(ser.read_all())
  
 #%% 
-rospy.init_node('thrustbalance_feedback') 
+rospy.init_node('thrustbalance_framework') 
 arduinoPublisher = rospy.Publisher('/tb/feedback', tb_feedback, queue_size=10)        
 fb_msg = tb_feedback()
 
-
-def arduino_publisher():      
-
-    # PWM = pwmGenerator
-    statring_time_offset = time.time()  
-    while not rospy.is_shutdown():
-        now =  rospy.get_time() - statring_time_offset
-        fb_msg.header.stamp = rospy.Time.from_sec(now)
+statring_time_offset = time.time()      
+def arduino_framework(cmd_vel_msg): 
+    
+    now =  rospy.get_time() - statring_time_offset
+    fb_msg.header.stamp = rospy.Time.from_sec(now)
+   
+    standard_message = "L"+ str(cmd_vel_msg.leftpwm)+"-"+ str(cmd_vel_msg.rightpwm)+"R/n"
+    ser.write("L"+ str(cmd_vel_msg.leftpwm)+"-"+ str(cmd_vel_msg.rightpwm)+"R\n")
         
-        # pwm_value = pwmgen()
-        
-        # ser.write("L",pwmgen,'-',pwmgen,'\n')
-        ser.write("L10-10R\n")
-        loop_delay_start = time.time()
-        while True:
-            sensor_data = ser.read_all() 
-            if sensor_data.endswith('R\n'):   
-                frequency = 1/(time.time()-loop_delay_start)                
-                break         
-        
-        fb_msg.frequency  = frequency         
-        fb_msg.roll = float(sensor_data.split("L")[0])
-        fb_msg.leftpwm = int(sensor_data[-7 : -5])
-        fb_msg.rightpwm = int(sensor_data[-4 : -2])
-        arduinoPublisher.publish(fb_msg)
-        print(fb_msg)
- 
+    loop_delay_start = time.time()
+    while True:
+        sensor_data = ser.read_all() 
+        if sensor_data.endswith('R\n'):   
+            frequency = 1/(time.time()-loop_delay_start)                
+            break      
+        elif sensor_data.startswith('w') :
+            print("wrong input data")
+            break        
+    
+    fb_msg.frequency  = frequency         
+    fb_msg.roll = float(sensor_data.split('L')[0])
+    fb_msg.leftpwm = int(sensor_data.split('L')[1].split('-')[0])
+    fb_msg.rightpwm = int(sensor_data.split('L')[1].split('-')[1].split('R')[0])
+    arduinoPublisher.publish(fb_msg)
+    print(fb_msg)
 
+    
 def main():
     
-    arduino_publisher()
+    arduinoSubscriber = rospy.Subscriber("/tb/cmd_pwm", tb_pwm, arduino_framework)
+    rospy.spin() 
 
 if __name__ == '__main__':
     try:
         main()
+        
        
     except rospy.ROSInterruptException:        
         pass
@@ -70,10 +72,10 @@ if __name__ == '__main__':
         rospy.signal_shutdown()
  
 # %% 
-
-print("L",pwmgen.lpwm,'-',pwmgen.rpwm,'\n')
-# %%
-pwm = tb_pwm()
-
+ 
+m = "-0.02L1-2R\n"
+k = m.split('L')
+print(m.split('L')[1].split('-')[0])
+print(m.split('L')[1].split('-')[1].split('R')[0])
 
 # %%
