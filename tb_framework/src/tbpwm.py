122d632e82f880f78da27e131e04086ace8dd88c
kasraazizi <kasraazizi1375@gmail.com>
2021-07-11 12:32:24 +0430

    first exprimental test modification

---
+++ tb_framework/src/tbpwm.py
@@ -0,0 +1,52 @@
+ 
+#%%
+#!/usr/bin/env python 
+import sys, select, tty, termios
+import rospy
+from tb_framework.msg import tb_pwm, tb_feedback
+#%% ROS inital
+ 
+rospy.init_node("pwmGenerator")
+pwmpublisher = rospy.Publisher("/tb/cmd_pwm", tb_pwm, queue_size=10)
+pwm_message = tb_pwm()
+rate = rospy.Rate(10)
+#%% pwm controller 
+
+def inpwm():
+    old_attr = termios.tcgetattr(sys.stdin)
+    tty.setcbreak(sys.stdin.fileno())
+    lpwm = 0
+    rpwm = 0
+    while True:
+        if select.select([sys.stdin], [], [], 0)[0] == [sys.stdin]:
+            key = sys.stdin.read(1)
+            if key == 'w':
+                lpwm = lpwm + 1
+                rpwm = rpwm + 1
+            
+            if key == 's':
+                lpwm = lpwm - 1
+                rpwm = rpwm - 1
+
+            if key == 'a':
+                lpwm = lpwm + 1
+                rpwm = rpwm - 1
+
+            if key == 'd':
+                lpwm = lpwm - 1
+                rpwm = rpwm + 1
+            
+            if key == 'q':
+                print("exit")
+                break
+
+        if lpwm <0: lpwm=0
+        if rpwm <0: rpwm=0
+         
+        pwm_message.leftpwm = lpwm
+        pwm_message.rightpwm = rpwm
+        pwmpublisher.publish(pwm_message)
+        rate.sleep()
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_attr)
+
+inpwm()
